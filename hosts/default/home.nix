71e1718b110e5e4cb24c747401ae2a89d42fbbb2
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-11-14 17:56:56 +1100

    Revert "Use homemanager systemd integration instead"
    This reverts commit 306ddde2107e6e8a1a4caf5bf83af2b90f120b7a.

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -87,7 +87,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    systemd.enable = true;
+    systemd.enable = false;
     settings = {
       "$mod" = "SUPER";
       "$mod_s" = "SUPER_SHIFT";
