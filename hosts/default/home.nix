cf2a6cd9c8d3cfb9f2036d5a17ba3f4d60dab84f
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-11-14 18:27:54 +1100

    Configure neovim as default again

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -68,7 +68,7 @@
   #  /etc/profiles/per-user/hamzah/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
