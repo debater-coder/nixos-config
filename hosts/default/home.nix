421c5701bfc092629e9366a21fa57f1524e2f546
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-11-21 21:51:58 +1100

    Install only office

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -18,6 +18,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    onlyoffice-desktopeditors
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
