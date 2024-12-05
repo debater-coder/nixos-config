a78cc99fa7f0ab7e025d48501a8bbf8115bd4fb9
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-12-06 08:36:49 +1100

    use jetbrains mono

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -299,6 +299,10 @@
     cursor.name = "Bibata-Modern-Classic";
    
     fonts = {
+      monospace = {
+         package = pkgs.jetbrains-mono;
+         name = "JetBrainsMono";
+      };
       sansSerif = {
         package = pkgs.rubik;
         name = "Rubik";
