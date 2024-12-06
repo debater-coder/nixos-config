490d725fd0d302cc47f7893117b742ab5c483d8e
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-12-06 19:00:58 +1100

    upate zen browser

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
