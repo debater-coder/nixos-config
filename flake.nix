9d1a780401ab1c210f8c00ebeef74788ddbdd5fd
debater-coder <52619668+debater-coder@users.noreply.github.com>
2024-12-22 16:38:56 +1100

    add winapps

--- flake.nix
+++ flake.nix
@@ -18,15 +18,29 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, winapps, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/default/configuration.nix
         inputs.home-manager.nixosModules.default
         inputs.stylix.nixosModules.stylix
+        (
+          { pkgs, ... }:
+          {
+              environment.systemPackages = [
+              winapps.packages.x86_64-linux.winapps
+              winapps.packages.x86_64-linux.winapps-launcher # optional
+              ];
+          }
+        )
       ];
     };
   };
