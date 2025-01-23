ee3ed69c0415050e2bb0d7a9c4750d2823b59991
debater-coder <hamzah@syedahmed.net>
2025-01-23 17:19:13 +1100

    silent boot

--- hosts/starscream/configuration.nix
+++ hosts/starscream/configuration.nix
@@ -13,7 +13,24 @@
     ];
 
   boot = {
+    plymouth = {
+      enable = true;
+    };
+
+    # Enable "Silent Boot"
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
     loader = {
+      timeout = 0;
       systemd-boot.enable = true;
       systemd-boot.configurationLimit = 5;
       systemd-boot.editor = false;
