287ea097166afe3f962a6d26b483d240615df8f7
debater-coder <hamzah@syedahmed.net>
2025-02-09 10:31:13 +1100

    fix display

--- hosts/starscream/home.nix
+++ hosts/starscream/home.nix
@@ -288,7 +288,7 @@
       monitor = [
         "eDP-1, preferred, 0x0, 1.6"  # laptop screen
         "desc:Dell Inc. DELL S2721QS DV61N43, preferred, 1600x0, 1.6"  # monitor left
-        "desc:Dell Inc. DELL S2721QS 5971N43, preferred, 4160x0, 1.6"  # monitor right
+        "desc:Dell Inc. DELL S2721QS 5971N43, preferred, 4000x0, 1.6"  # monitor right
         ", preferred, auto, 1"  # random monitors
         "monitor=FALLBACK,1920x1080@60,auto,1" # fix hyprlock
       ];
