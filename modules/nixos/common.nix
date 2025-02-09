b122d7b33218b284f7770e202700ece0713d4140
debater-coder <hamzah@syedahmed.net>
2025-02-10 08:49:59 +1100

    fix ntp

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -1,7 +1,7 @@
 {
   # Set your time zone.
   time.timeZone = "Australia/Sydney";
-  time.hardwareClockInLocalTime = false;
+  time.hardwareClockInLocalTime = true;
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_AU.UTF-8";
