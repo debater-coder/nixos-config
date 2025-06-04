e12a7c90b3f14adf847ebc5c1c8f6accbdb6fd2e
debater-coder <hamzah@syedahmed.net>
2025-06-04 16:38:53 +1000

    lean things

--- hosts/starscream/configuration.nix
+++ hosts/starscream/configuration.nix
@@ -209,6 +209,13 @@
   virtualisation.spiceUSBRedirection.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.extraOptions = "
+  max-jobs = auto  # Allow building multiple derivations in parallel
+  keep-outputs = true  # Do not garbage-collect build time-only dependencies (e.g. clang)
+  # Allow fetching build results from the Lean Cachix cache
+  trusted-substituters = https://lean4.cachix.org/
+  trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= lean4.cachix.org-1:mawtxSxcaiWE24xCXXgh3qnvlTkyU7evRRnGeAhD4Wk=
+  ";
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
